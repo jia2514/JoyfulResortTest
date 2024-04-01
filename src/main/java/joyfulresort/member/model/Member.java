5e4e953fbecfdd0bb0571d8756c902d8f365ceb2
shun <ool435716@gmail.com>
2024-04-01 15:32:57 +0800

    ????

---
+++ src/main/java/joyfulresort/member/model/Member.java
@@ -0,0 +1,20 @@
+package joyfulresort.member.model;
+
+public class Member {
+	private Integer memNo;
+	private String memName;
+	public Integer getMemNo() {
+		return memNo;
+	}
+	public void setMemNo(Integer memNo) {
+		this.memNo = memNo;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	
+	
+}
