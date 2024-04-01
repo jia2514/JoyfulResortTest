933c1a9e7cd73425a294752e03a17fd8f4c290cb
Yuhan <jasmine940703@gmail.com>
2024-04-01 15:30:26 +0800

    first commit

--- src/main/java/com/tiabame/joyfulresorttest/Member.java
+++ src/main/java/com/tiabame/joyfulresorttest/Member.java
@@ -1,5 +1,20 @@
 package com.tiabame.joyfulresorttest;
 
 public class Member {
-
+	private Integer memNo;
+	private String memName;
+	public Integer getMemNo() {
+		return memNo;
+	}
+	public void setMemNo(Integer memNo) {
+		this.memNo = memNo;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	
+	
 }
