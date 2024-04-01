2c49664fd47c39041784616efb7aca8b695592da
Bin <xd890872680@gmail.comgit config --listgit config --listfgit config --global user.email>
2024-04-01 15:32:58 +0800

    update member

--- src/main/java/com/tibame/mytest/Member.java
+++ src/main/java/com/tibame/mytest/Member.java
@@ -4,22 +4,23 @@ public class Member {
 
 	
 
-		private int no;
-		public int getNo() {
-			return no;
-		}
-		public void setNo(int no) {
-			this.no = no;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		private String name;
+	private int  no;
+	private String  name;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 		
 	
 	}
 
-}
+
